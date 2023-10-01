87af2276c4801a66f9dc9028d182e5b4ce74daca
Richard Ockerby <richard.ockerby@drummondcentral.co.uk>
2023-10-01 15:17:41 +0100

    Add version specific install to Umbraco CLI

--- DevSetup.bat
+++ DevSetup.bat
@@ -1,7 +1,7 @@
 cd src
 REM This tool assumes that Umbraco Templates is already installed on the target machine. Running the below line would overwrite anyone's current installed version, that sounds cheeky...
-REM dotnet new install Umbraco.Templates::10.4.0
-dotnet new umbraco -n FusionCacheTools.TestSite --friendly-name "Administrator" --email "admin@example.com" --password "1234567890" --development-database-type SQLite
+REM dotnet new install Umbraco.Templates::10.7.0
+dotnet new umbraco --version "10.7.0" -n FusionCacheTools.TestSite --friendly-name "Administrator" --email "admin@example.com" --password "1234567890" --development-database-type SQLite
 dotnet add FusionCacheTools.TestSite\FusionCacheTools.TestSite.csproj reference FusionCacheTools.BackOffice\FusionCacheTools.BackOffice.csproj
 
 echo "Done"
